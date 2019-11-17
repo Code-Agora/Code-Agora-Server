bd605237b55377c86f87cdaeeeeb46291efe2850
Claro614 <rochacl@appstate.edu>
2019-11-17 17:01:39 -0500

    Updated User and questions

--- migrations/01-User.js
+++ migrations/01-User.js
@@ -4,6 +4,7 @@ exports.up = function(knex, Promise) {
         table.increments('id');
         table.string('username', 100).notNullable();
         table.datetime('date_joined').notNullable();
+        table.integer('reputation').notNullable();
         table.string('lastname', 100);
         table.string('firstname', 100);
         table.string('email', 100).notNullable();
--- migrations/02-Question.js
+++ migrations/02-Question.js
@@ -2,10 +2,11 @@
 exports.up = function(knex, Promise) {
 	return knex.schema.createTable('Question', (table) => {
         table.increments('id');
+        table.integer('votes');
         table.integer('user_id');
         table.foreign('user_id').references('_User.id').onDelete('CASCADE');
         table.integer('view_count').defaultTo(0);
-        table.string('title', 70).notNullable();
+        table.text('title').notNullable();
         table.text('body').notNullable();
 	});
 };
